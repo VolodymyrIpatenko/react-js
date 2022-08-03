5c331618828f40b64f6515bfe7fe176a20e1aff2
VolodymyrIpatenko <81745231+VolodymyrIpatenko@users.noreply.github.com>
2022-08-03 14:53:25 +0300

    c

---
+++ react/lesson2/task1/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const rootElement = document.querySelector('#root');
+
+const element = (
+  <div>
+  <h1>Search Form</h1>
+  <div>
+  <input type="text" />
+  <button>Search</button>
+  </div>
+  </div>
+  );
+  
+  ReactDOM.render(element, rootElement);
+  
