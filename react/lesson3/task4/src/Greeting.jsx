12c764fa6508cfa3146099f952a4c7357d8b3220
VolodymyrIpatenko <81745231+VolodymyrIpatenko@users.noreply.github.com>
2022-08-06 14:10:50 +0300

    c

--- react/lesson3/task4/src/Greeting.jsx
+++ react/lesson3/task4/src/Greeting.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+const yearOfBirth = new Date("2005-01-01").getFullYear();
+const currentYear = new Date().getFullYear();
+const birthDate = currentYear - yearOfBirth;
+
 export default function Greeting(props) {
 	return (
 		<div className="greeting">
