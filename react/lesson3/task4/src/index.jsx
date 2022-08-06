8d007eb6662867b67d4135785946db11983a5cf0
VolodymyrIpatenko <81745231+VolodymyrIpatenko@users.noreply.github.com>
2022-08-06 14:15:31 +0300

    c

--- react/lesson3/task4/src/index.jsx
+++ react/lesson3/task4/src/index.jsx
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom';
 import "./index.scss";
 import Greeting from "./Greeting.jsx";
 
-const yearOfBirth = new Date("2005-01-01").getFullYear();
-const currentYear = new Date().getFullYear();
-const birthDate = currentYear - yearOfBirth;
 
 const rootElement = document.querySelector('#root');
 	
-ReactDOM.render(<Greeting firstName="John" lastName="Doe" birthDate={birthDate} />, rootElement);
+ReactDOM.render(<Greeting firstName="John" lastName="Doe" birthDate={new Date().getFullYear() -new Date("2005-01-01").getFullYear()} />, rootElement);
  
