65cfa528d0b65f6d41279f2d6fcac6240ca951ef
VolodymyrIpatenko <81745231+VolodymyrIpatenko@users.noreply.github.com>
2022-08-07 12:19:20 +0300

    c

--- react/lesson4/task1/src/App.jsx
+++ react/lesson4/task1/src/App.jsx
@@ -3,7 +3,11 @@ import Counter from "./Counter.jsx";
 
 const App = () => {
 	return (
-		<Counter/>
+		<>
+			<Counter start={5} interval={1000} />
+			<Counter start={12} interval={100} />
+			<Counter start ={-21} interval={1000} />
+		</>
 	);
 }
 
