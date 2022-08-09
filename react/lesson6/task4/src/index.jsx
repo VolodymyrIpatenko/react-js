43dc35bf885c6d4db63a1629ea7233956fd9383b
VolodymyrIpatenko <81745231+VolodymyrIpatenko@users.noreply.github.com>
2022-08-09 16:00:21 +0300

    c

--- react/lesson6/task4/src/index.jsx
+++ react/lesson6/task4/src/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDom from "react-dom";
 import "./index.scss";
-import Status from "./Status.jsx";
+import Auth from "./Auth.jsx";
 
 const rootElement = document.querySelector("#root");
 
-ReactDom.render(<Status/>, rootElement);
+ReactDom.render(<Auth/>, rootElement);
