313d2c2cf2c2b55c4be10d12c6b53f7c967a7995
VolodymyrIpatenko <81745231+VolodymyrIpatenko@users.noreply.github.com>
2022-08-15 14:04:52 +0300

    с

--- react/lesson8/task1/src/Life.jsx
+++ react/lesson8/task1/src/Life.jsx
@@ -1,34 +1,38 @@
-import React, { Component } from 'react';
+import React from "react";
 
-class Life extends Component {
-	constructor(props) {
-		super(props);
-		console.log("constructor: good place to create state");
-	}
+class Life extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("constructor: good place to create state");
+  }
 
-	componentDidMount() {
-		console.log("componentDidMount: API calls, subscriptions");
-	}
+  componentDidMount() {
+    console.log("componentDidMount: API calls, subscriptions");
+  }
 
-	shouldComponentUpdate(nextProps, nextState) {
-		console.log(nextProps,nextState);
-		console.log("shouldComponentUpdate(nextProps,nextState):decide to render or not to render");
-	}
-	
-	componentDidUpdate(prevProps, prevState) {
-		this.props
-		console.log(prevProps);
-		console.log("componentDidUpdate(prevProps,prevState):some updates based on new props");
-	}
+  componentWillUnmount() {
+    console.log(
+      "componentWillUnmount(): cleanup before DOM related to component will be removed"
+    );
+  }
 
-	componentWillUnmount() {
-		console.log("componentWillUnmount():cleanup before DOM related to component will be removed");
-	}
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log(
+      "shouldComponentUpdate(nextProps, nextState): decide to render or not to render"
+    );
+    return true;
+  }
 
-	render() {
-		console.log("return React element to build DOM")
-		return <div className="number">{this.props}</div>
-	}
+  componentDidUpdate() {
+    console.log(
+      "componentDidUpdate(prevProps, prevState): some updates based on new props"
+    );
+  }
+
+  render() {
+    console.log("return React element to build DOM");
+    return <div>{this.props.number}</div>;
+  }
 }
 
 export default Life;
