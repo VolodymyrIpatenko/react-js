80973e22eceb52b83cf785f43aa64bc857ccf1e6
VolodymyrIpatenko <81745231+VolodymyrIpatenko@users.noreply.github.com>
2022-08-16 14:49:11 +0300

    c

--- react/lesson9/task2/src/UserForm.jsx
+++ react/lesson9/task2/src/UserForm.jsx
@@ -20,7 +20,7 @@ class UserForm extends Component {
 	
 	render() {
 		return (
-			<form className="login-form" onSubmit={this.handleSubmit}>
+			<form className="login-form" onSubmit={this.handleChange}>
 				<h1 className="form-title">Profile</h1>
 				<div className="form-control">
 					<label className="form-label" htmlFor="name">Name</label>
