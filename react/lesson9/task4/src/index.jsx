55ebb36b20c9b1da04a74b4a3e77575bde75e43a
VolodymyrIpatenko <81745231+VolodymyrIpatenko@users.noreply.github.com>
2022-08-18 11:27:22 +0300

    с

--- react/lesson9/task4/src/index.jsx
+++ react/lesson9/task4/src/index.jsx
@@ -1,6 +1,6 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import Game from ".Game/.jsx";
-// const rootElement = document.querySelector("#root");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Game from './Game';
+import './index.scss';
 
-// ReactDOM.render(<Game/>, rootElement );
+ReactDOM.render(<Game />, document.querySelector('#root'));
